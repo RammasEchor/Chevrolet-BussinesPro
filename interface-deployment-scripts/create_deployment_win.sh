6d6d131e9a4ac2a2c1a97187cbd125e59fa99d84
Rammas <luis.nieto.palacios1996@gmail.com>
2023-09-21 21:07:02 -0600

    New script for creating deploy

--- interface-deployment-scripts/create_deployment_win.sh
+++ interface-deployment-scripts/create_deployment_win.sh
@@ -2,6 +2,6 @@
 
 cp /home/rammas/repos/Chevrolet_DMS_Watcher/bin/Debug/net7.0/win-x64/publish/Chevrolet_DMS_Watcher.exe ./
 cp /home/rammas/repos/chevrolet-businessPro/interface-api-businesspro/bin/Debug/net7.0/win-x64/publish/interface-api-businesspro.exe ./
-zip deployment.zip Chevrolet_DMS_Watcher.exe interface-api-businesspro.exe *.bat
+zip -9 -y -r deployment.zip Chevrolet_DMS_Watcher.exe interface-api-businesspro.exe *.bat
 rm ./Chevrolet_DMS_Watcher.exe
 rm ./interface-api-businesspro.exe
