30b4cd91aaf29a7531ba1a00f5c4a0d52f9fedb1
Rammas <luis.nieto.palacios1996@gmail.com>
2023-10-14 16:00:45 -0600

    Change the configuration of publish to Release
    This will also remove the code-gen project from build/release tasks.

--- interface-deployment-scripts/create_deployment_win.sh
+++ interface-deployment-scripts/create_deployment_win.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm ./deployment.zip
 cp /home/rammas/repos/Chevrolet_DMS_Watcher/bin/Debug/net7.0/win-x86/publish/Chevrolet_DMS_Watcher.exe ./
-cp /home/rammas/repos/chevrolet-businessPro/interface-api-businesspro/bin/Debug/net7.0/win-x86/publish/interface-api-businesspro.exe ./
+cp /home/rammas/repos/chevrolet-businessPro/interface-api-businesspro/bin/Release/net7.0/win-x86/publish/interface-api-businesspro.exe ./
 zip -9 -y -r deployment.zip Chevrolet_DMS_Watcher.exe interface-api-businesspro.exe *.bat
 rm ./Chevrolet_DMS_Watcher.exe
 rm ./interface-api-businesspro.exe
